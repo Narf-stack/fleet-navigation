cfca1814608306b5c5646defc3cf5cf39f48377b
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-11-07 18:11:02 +0100

    Add index on id field for Vehicule

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_07_162523) do
+ActiveRecord::Schema.define(version: 2021_11_07_170815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_11_07_162523) do
   create_table "vehicles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["id"], name: "index_vehicles_on_id", unique: true
   end
 
   add_foreign_key "locations", "vehicles"
