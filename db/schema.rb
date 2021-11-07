761f9d466b71da908b5f03b084fb998ffe5fcfad
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-11-07 18:21:33 +0100

    Add field Taken to Vehicule model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_07_170815) do
+ActiveRecord::Schema.define(version: 2021_11_07_171933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_11_07_170815) do
   create_table "vehicles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "taken", default: false
     t.index ["id"], name: "index_vehicles_on_id", unique: true
   end
 
